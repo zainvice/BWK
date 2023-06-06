6bff5e80c31c6c666c93b58bc3fa3968e569a9a1
zainvice <amazwings@gmail.com>
2023-06-06 11:28:14 -0700

    Responsive for Customer not fully yet!

--- src/pages/CustomerHomePage.js
+++ src/pages/CustomerHomePage.js
@@ -5,26 +5,36 @@ const CustomerHomePage = () => {
 
   return (
     <div className={styles.customerHomePage}>
-      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+     
       <CHeader/>
+      <div className={styles.homeTop}>
+      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+      
       <b className={styles.welcomeToBetContainer}>
+        <span className={styles.customerName}>CUSTOMER NAME</span>
         <span>{`WELCOME TO `}</span>
         <span className={styles.betWiseKenya}>BET WISE KENYA!</span>
       </b>
-      <div className={styles.customerHomePageChild} />
-      <b className={styles.latestTips}>
-        <span>{`LATEST `}</span>
-        <span className={styles.tips}>TIPS</span>
-      </b>
+      <div className={styles.vectorParent}>
+      <b className={styles.tips}>
+        <span>{`LATEST TIPS`}</span>
+        </b>
+      </div>
+      <div  />
+      
+      </div>
+      <div className={styles.homDescription}>
+        
+     
       <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-      <b className={styles.customerName}>CUSTOMER NAME</b>
-      <div className={styles.vectorParent} >
-        <img className={styles.groupChild} alt="" src="/rectangle-32.svg" />
+     
+      <div className={styles.vectorGroup} >
         <b className={styles.viewLatestTips}>View Latest Tips</b>
       </div>
       
       <b className={styles.postedTipsWill}>Posted Tips will show here!</b>
       
+      </div>
       
     </div>
   );
--- src/pages/CustomerSUBPage.js
+++ src/pages/CustomerSUBPage.js
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Subscriptions.module.css";
 import CHeader from "../components/CustomerHeader";
+import Subscriptions_show from "../components/Subscription_show";
 const CustomerSUBPage = () => {
   const navigate = useNavigate();
 
@@ -42,21 +43,16 @@ const CustomerSUBPage = () => {
   }, [navigate]);
 
   return (
+    <>
     <div className={styles.customerSubPage}>
       <CHeader/>
-      
-      <b className={styles.subscriptions1} onClick={onSubscriptionsText1Click}>
+      <div className={styles.subscriptions2}>
+      <b  onClick={onSubscriptionsText1Click}>
         Subscriptions
       </b>
-      <b
-        className={styles.upgradeYourSubscriptionContainer}
-        onClick={onUpgradeYourSubscriptionClick}
-      >
-        <span>Upgrade your</span>
-        <span className={styles.subscription}> Subscription</span>
-      </b>
+      </div>
       <div
-        className={styles.youreCurrentlySubscribedContainer}
+        className={styles.readyToTakeContainer}
         onClick={onYoureCurrentlySubscribedClick}
       >
         <span>
@@ -67,184 +63,19 @@ const CustomerSUBPage = () => {
           <b>Super Multi-Bet!</b>
         </span>
       </div>
-      <div className={styles.subscriptionOptions}>
-        <img className={styles.image5Icon} alt="" src="/image-5@2x.png" />
-        <div className={styles.smsTipsOnly}>SMS TIPS ONLY</div>
-        <div className={styles.smswebTips}>{`SMS+WEB TIPS `}</div>
-        <div className={styles.smswebTips1}>{`SMS+WEB TIPS `}</div>
-        <div className={styles.smswebTips2}>{`SMS+WEB TIPS `}</div>
-        <div className={styles.bronze}>
-          <div className={styles.bronzeOneDayContainer}>
-            <p className={styles.bronzeOneDay}>
-              <b>Bronze (One day).</b>
-            </p>
-            <ul className={styles.x2Ov15un15Ov25un25B}>
-              <li className={styles.btts}>
-                <span>{`1X2                                                       `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV1.5/UN1.5                                                 `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV2.5/UN2.5                                      `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`BTTS                                                                  `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`HT/FT                                                                  `}</span>
-              </li>
-              <li>
-                <span>{`Super Multibet                                                  `}</span>
-              </li>
-            </ul>
-          </div>
-          <b className={styles.type}>TYPE</b>
-          <b className={styles.price}>PRICE</b>
-          <b className={styles.offer}>OFFER</b>
-          <img className={styles.vectorIcon} alt="" src="/vector1.svg" />
-          <div className={styles.ksh100KshContainer}>
-            <p className={styles.bronzeOneDay}>Ksh 100</p>
-            <p className={styles.bronzeOneDay}>Ksh 100</p>
-            <p className={styles.bronzeOneDay}>Ksh 150</p>
-            <p className={styles.bronzeOneDay}>Ksh 150</p>
-            <p className={styles.bronzeOneDay}>Ksh 200</p>
-            <p className={styles.bronzeOneDay}>Ksh 300</p>
-          </div>
-        </div>
-        <div className={styles.silver}>
-          <div className={styles.silverSevenDaysContainer}>
-            <p className={styles.bronzeOneDay}>
-              <b>Silver (Seven days tips):</b>
-            </p>
-            <ul className={styles.x2Ov15un15Ov25un25B}>
-              <li className={styles.btts}>
-                <span>{`1X2                                             `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV1.5/UN1.5                                        `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV2.5/UN2.5                              `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`BTTS                                   `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`HT/FT                            `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`Super Multibet                                                `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`Mega jackpot analysis                               `}</span>
-              </li>
-              <li>
-                <span>{`Mid-week jackpot analysis                  `}</span>
-              </li>
-            </ul>
-          </div>
-          <img className={styles.vectorIcon1} alt="" src="/vector2.svg" />
-          <div className={styles.ksh600KshContainer}>
-            <p className={styles.bronzeOneDay}>Ksh 600</p>
-            <p className={styles.bronzeOneDay}>Ksh 600</p>
-            <p className={styles.bronzeOneDay}>Ksh 900</p>
-            <p className={styles.bronzeOneDay}>Ksh 900</p>
-            <p className={styles.bronzeOneDay}>Ksh 1200</p>
-            <p className={styles.bronzeOneDay}>Ksh 1800</p>
-            <p className={styles.bronzeOneDay}>{`Ksh 300 `}</p>
-            <p className={styles.bronzeOneDay}>{`Ksh 200 `}</p>
-          </div>
-          <div className={styles.onceAWeek}> (Once a week)</div>
-          <div className={styles.onceAWeek1}> (Once a week)</div>
-        </div>
-        <div className={styles.gold1}>
-          <div className={styles.gold14DaysContainer}>
-            <p className={styles.bronzeOneDay}>
-              <b>Gold (14 days tips):</b>
-            </p>
-            <ul className={styles.x2Ov15un15Ov25un25B}>
-              <li className={styles.btts}>
-                <span>1X2</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV1.5/UN1.5 `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`OV2.5/UN2.5 `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`BTTS `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`HT/FT `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`Super Multibet `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>Mega jackpot analysis</span>
-              </li>
-              <li>
-                <span>Mid-week jackpot analysis</span>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.ksh600KshContainer}>
-            <p className={styles.bronzeOneDay}>Ksh 1000</p>
-            <p className={styles.bronzeOneDay}>Ksh 1000</p>
-            <p className={styles.bronzeOneDay}>Ksh 1500</p>
-            <p className={styles.bronzeOneDay}>Ksh 1500</p>
-            <p className={styles.bronzeOneDay}>Ksh 2000</p>
-            <p className={styles.bronzeOneDay}>Ksh 3000</p>
-            <p className={styles.bronzeOneDay}>{`Ksh 500 `}</p>
-            <p className={styles.bronzeOneDay}>{`Ksh 300 `}</p>
-          </div>
-          <div className={styles.onceAWeek2}>(Once a week)</div>
-          <div className={styles.onceAWeek3}>(Once a week)</div>
-          <img className={styles.vectorIcon2} alt="" src="/vector3.svg" />
-        </div>
-        <div className={styles.vip}>
-          <div className={styles.ksh3000KshContainer}>
-            <p className={styles.bronzeOneDay}>Ksh 3000</p>
-            <p className={styles.bronzeOneDay}>Ksh 5000</p>
-            <p className={styles.bronzeOneDay}>Ksh 8000</p>
-          </div>
-          <div className={styles.vipWeekly14Container}>
-            <p className={styles.bronzeOneDay}>
-              <b className={styles.vip2}>VIP</b>
-              <span>{` `}</span>
-            </p>
-            <ul className={styles.weekly14Days30Days}>
-              <li className={styles.btts}>
-                <span>Weekly</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`14 days `}</span>
-              </li>
-              <li className={styles.btts}>
-                <span>{`30 days `}</span>
-              </li>
-            </ul>
-            <p className={styles.bronzeOneDay}>
-              Access to all platinum features +
-            </p>
-            <ul className={styles.weekly14Days30Days}>
-              <li className={styles.btts}>
-                <span>Weekly smasher bet</span>
-              </li>
-              <li className={styles.btts}>
-                <span>Mega jackpot analysis</span>
-              </li>
-              <li>
-                <span>Mid-week jackpot analysis</span>
-              </li>
-            </ul>
-          </div>
-          <img className={styles.vectorIcon3} alt="" src="/vector4.svg" />
-        </div>
+      <div  className={styles.whatWeAreContainer}>
+      <b
+       
+        onClick={onUpgradeYourSubscriptionClick}
+      >
+        <span>Upgrade your</span>
+        <span className={styles.subscription}> Subscription</span>
+      </b>
       </div>
+     
     </div>
+    <Subscriptions_show/>
+    </>
   );
 };
 

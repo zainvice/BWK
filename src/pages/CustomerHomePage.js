05b873f1da6979772c45a98d869d8286570707ff
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-06-07 14:35:07 +0500

    Styled customer home-page

--- src/pages/CustomerHomePage.js
+++ src/pages/CustomerHomePage.js
@@ -1,41 +1,34 @@
-
 import styles from "./CustomerHomePage.module.css";
 import CHeader from "../components/CustomerHeader";
 const CustomerHomePage = () => {
-
   return (
     <div className={styles.customerHomePage}>
-     
-      <CHeader/>
+      <CHeader />
       <div className={styles.homeTop}>
-      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
-      
-      <b className={styles.welcomeToBetContainer}>
-        <span className={styles.customerName}>CUSTOMER NAME</span>
-        <span>{`WELCOME TO `}</span>
-        <span className={styles.betWiseKenya}>BET WISE KENYA!</span>
-      </b>
-      <div className={styles.vectorParent}>
-      <b className={styles.tips}>
-        <span>{`LATEST TIPS`}</span>
-        </b>
-      </div>
-      <div  />
-      
+        <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+        <div className={styles.welcomeToBetContainer}>
+          <h3 className={styles.customerName}>CUSTOMER NAME</h3>
+          <h1 className={styles.betWiseKenya}>
+            {`WELCOME TO `}
+            <span>BET WISE KENYA!</span>
+          </h1>
+          <div>
+            <button className={styles.submit}>View Latest Tips</button>
+          </div>
+        </div>
       </div>
       <div className={styles.homDescription}>
-        
-     
-      <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-     
-      <div className={styles.vectorGroup} >
-        <b className={styles.viewLatestTips}>View Latest Tips</b>
+        <div className={styles.homemDescriptionLeft}>
+          <h1 className={styles.latestTips}>LATEST <span>TIPS</span></h1>
+
+          <div className={styles.postedTips}>
+            <p>Posted tips will show here</p>
+          </div>
+        </div>
+        <div className={styles.homDescriptionRight}>
+          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+        </div>
       </div>
-      
-      <b className={styles.postedTipsWill}>Posted Tips will show here!</b>
-      
-      </div>
-      
     </div>
   );
 };

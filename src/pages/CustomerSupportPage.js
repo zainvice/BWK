76ec65e80cfe1519eff151f917caba6ce87b8171
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-06-07 22:16:53 +0500

    Styled customer support page and request recieved page

--- src/pages/CustomerSupportPage.js
+++ src/pages/CustomerSupportPage.js
@@ -1,48 +1,71 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./CustomerSupportPage.module.css";
 import CHeader from "../components/CustomerHeader";
+import styles from "./CustomerSupportPage.module.css";
+import contactStyles from "./ContactUs.module.css";
+
 const CustomerSupportPage = () => {
   const navigate = useNavigate();
-
 
   const onLOGINContainerClick = useCallback(() => {
     navigate("/cust-request-recieved");
   }, [navigate]);
 
-  
-
   return (
     <div className={styles.customerSupportPage}>
-     <CHeader/>
-      <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
-      <div className={styles.loginForm}>
-        <div className={styles.loginFormChild} />
-        <div className={styles.loginFormItem} />
-        <div className={styles.description}>Description</div>
-        <div className={styles.loginFormInner} />
-        <div className={styles.subject}>Subject</div>
-        <div className={styles.summarizeWhatHappened}>
-          Summarize what happened....
-        </div>
-        <div className={styles.describeYourIssue}>Describe your issue...</div>
-        <div className={styles.submitYourComplaint}>
-          Submit your complaint or concern, we’ll get to you shortly
-        </div>
-        <div className={styles.orReachUsContainer}>
-          <p className={styles.or}>
-            <b>OR</b>
+      <CHeader />
+
+      <div className={contactStyles.formContainer}>
+        <img className={contactStyles.formImg} alt="" src="/bulb.svg" />
+
+        <form className={contactStyles.form}>
+          <h1>Support</h1>
+          <div className={contactStyles.inputContainer}>
+            <label className={contactStyles.inputlabel}>
+              Subject
+              <input
+                className={contactStyles.input}
+                type="text"
+                placeholder="Summarize what happened..."
+              />
+            </label>
+          </div>
+
+          <div className={contactStyles.inputContainer}>
+            <label className={contactStyles.inputlabel}>
+              Description
+              <textarea
+                cols={10}
+                rows={10}
+                className={contactStyles.input + " " + contactStyles.inputArea}
+                placeholder="Describe your issue..."
+              />
+            </label>
+          </div>
+
+          <p className={contactStyles.complaintText + " " + styles.willGetYou}>
+            {"Submit your complaint or concern, we’ll get to you shortly"}
           </p>
-          <p className={styles.or}>Reach us on</p>
-        </div>
-        <div className={styles.login} onClick={onLOGINContainerClick}>
-          <div className={styles.loginChild} />
-          <b className={styles.submit}>SUBMIT</b>
-        </div>
-        <b className={styles.support} >
-          Support
-        </b>
-        <img className={styles.groupIcon} alt="" src="/group-4.svg" />
+
+          <div className={contactStyles.buttonContainer}>
+            <button className={contactStyles.submit}>SUBMIT</button>
+          </div>
+
+          <p className={contactStyles.complaintText + " " + styles.orReachUs}>
+            <b>OR</b> {"\n"} Reach us on
+          </p>
+          <div className={styles.reachUsOnLinks}>
+            <a>
+              <img className={styles.reachUsOnImg} src="/phone.svg" />
+            </a>
+            <a>
+              <img className={styles.reachUsOnImg} src="/whatsapp.svg" />
+            </a>
+            <a>
+              <img className={styles.reachUsOnImg} src="/send.svg" />
+            </a>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/RequestRecieved.js
+++ src/pages/RequestRecieved.js
@@ -1,41 +1,11 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./RequestRecieved.module.css";
 import Header from "../components/Header";
+import contactStyles from "./ContactUs.module.css";
+import styles from "./RequestRecieved.module.css";
+
 const RequestRecieved = () => {
   const navigate = useNavigate();
-
-  const onSignupContainerClick = useCallback(() => {
-    navigate("/signup");
-  }, [navigate]);
-
-  const onLOginContainerClick = useCallback(() => {
-    navigate("/login");
-  }, [navigate]);
-
-  const onHomeTextClick = useCallback(() => {
-    navigate("/homepage");
-  }, [navigate]);
-
-  const onSubscriptionsTextClick = useCallback(() => {
-    navigate("/subscriptions");
-  }, [navigate]);
-
-  const onSMSBettingTextClick = useCallback(() => {
-    navigate("/sms-betting");
-  }, [navigate]);
-
-  const onWebBettingTextClick = useCallback(() => {
-    navigate("/web-betting");
-  }, [navigate]);
-
-  const onLiveScoreTextClick = useCallback(() => {
-    navigate("/live-score");
-  }, [navigate]);
-
-  const onContactUsTextClick = useCallback(() => {
-    navigate("/");
-  }, [navigate]);
 
   const onSupportTextClick = useCallback(() => {
     navigate("/sms-betting");
@@ -43,26 +13,19 @@ const RequestRecieved = () => {
 
   return (
     <>
-    <div className={styles.requestRecieved}>
-     <Header/>
-      <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
-      <div className={styles.loginForm}>
-        <div className={styles.loginFormChild} />
-        <b className={styles.support} onClick={onSupportTextClick}>
-          Support
-        </b>
+      <div className={styles.requestRecieved}>
+        <Header />
+        <div className={contactStyles.formContainer}>
+          <img className={contactStyles.formImg} alt="" src="/bulb.svg" />
+          <div className={contactStyles.form}>
+            <h1>Support</h1>
+            <p className={styles.weRecievedMsg}>
+              {"We have "}<span>received</span> you request, we’ll reach out soon! Feel free
+              to browse our site till then!
+            </p>
+          </div>
+        </div>
       </div>
-      <div className={styles.weHaveReceivedContainer}>
-        <p className={styles.weHaveReceivedYouRequest}>
-          <span className={styles.weHave}>{`We have `}</span>
-          <span className={styles.received}>received</span>
-          <span> you request, we’ll reach out soon!</span>
-        </p>
-        <p className={styles.weHaveReceivedYouRequest}>
-          Feel free to browse our site till then!
-        </p>
-      </div>
-    </div>
     </>
   );
 };

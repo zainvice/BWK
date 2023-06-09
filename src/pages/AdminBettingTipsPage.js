beccfda222b24059ad86b314bb81a73337f02b83
zainvice <amazwings@gmail.com>
2023-06-09 08:36:46 -0700

    .

--- src/pages/AdminBettingTipsPage.js
+++ src/pages/AdminBettingTipsPage.js
@@ -1,6 +1,6 @@
 import AdminContainer from "../components/AdminContainer";
 import AdminContent from "../components/AdminContent";
-import styles from "./AdminBettingTipsPage.module.css";
+
 import AdminSideBar from "../components/AdminSideBar";
 const AdminBettingTipsPage = () => {
   return (
